b5dd078bed07752547ec14ebd918233ce6e29490
marisabel <varzieva36@gmail.com>
2026-04-13 17:47:11 +0300

    Update about_us_screen.dart

--- lib/screens/about_us_screen.dart
+++ lib/screens/about_us_screen.dart
@@ -1,1 +1,41 @@
+import 'package:flutter/material.dart';
 
+class AboutUsScreen extends StatelessWidget {
+  const AboutUsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          const Center(
+            child: Text('ABOUT US Screen'),
+          ),
+
+///EXIT
+          Positioned(
+            left: 20,
+            top: 40,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.4),
+                  shape: BoxShape.circle,
+                ),
+                child: const Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
